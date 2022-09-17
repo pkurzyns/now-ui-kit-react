cfd39743030d3910425bd4082e251183679dafcc
pkurzyns <p.kurzyns@gmail.com>
2022-09-17 22:33:33 +0200

    First commit
    Small changes of an images

--- src/components/Footers/TransparentFooter.js
+++ src/components/Footers/TransparentFooter.js
@@ -36,23 +36,6 @@ function TransparentFooter() {
             </li>
           </ul>
         </nav>
-        <div className="copyright" id="copyright">
-          © {new Date().getFullYear()}, Designed by{" "}
-          <a
-            href="https://www.invisionapp.com?ref=nukr-transparent-footer"
-            target="_blank"
-          >
-            Invision
-          </a>
-          . Coded by{" "}
-          <a
-            href="https://www.creative-tim.com?ref=nukr-transparent-footer"
-            target="_blank"
-          >
-            Creative Tim
-          </a>
-          .
-        </div>
       </Container>
     </footer>
   );
--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -28,7 +28,7 @@ function IndexHeader() {
         <div
           className="page-header-image"
           style={{
-            backgroundImage: "url(" + require("assets/img/header.jpg") + ")"
+            backgroundImage: "url(" + require("assets/img/industrial-plant-sawmill-storage-of-wooden-boards-3869084.jpeg") + ")"
           }}
           ref={pageHeader}
         ></div>
