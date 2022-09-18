9456d1bf182ca3f2176915120298e73ebff773e1
pkurzyns <p.kurzyns@gmail.com>
2022-09-18 11:08:22 +0200

    Add package-lock.json

--- src/views/index-sections/Images.js
+++ src/views/index-sections/Images.js
@@ -12,8 +12,24 @@ function Images() {
         <Container>
           <Row>
             <Col md="12">
-            
-            
+              <div className="hero-images-container">
+                <img
+                  alt="..."
+                  src={require("assets/img/hero-image-1.png")}
+                ></img>
+              </div>
+              <div className="hero-images-container-1">
+                <img
+                  alt="..."
+                  src={require("assets/img/hero-image-2.png")}
+                ></img>
+              </div>
+              <div className="hero-images-container-2">
+                <img
+                  alt="..."
+                  src={require("assets/img/hero-image-3.png")}
+                ></img>
+              </div>
             </Col>
           </Row>
         </Container>
